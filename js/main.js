74b69bf4486d888589fa02e8783c9b741ac3957d
mikemet <mikemet@outlook.com>
2020-12-29 04:33:48 +0300

    adding structure of site and all files and folders

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// button "TO TOP"
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function scrollToTop () {
+rootElement.scrollTo({
+    top: 0, 
+    behavior: "smooth"
+})
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
