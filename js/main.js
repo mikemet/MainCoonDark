e3c370fd98948555a08b12f277818d8e503d3fc2
mikemet <mikemet@outlook.com>
2020-12-31 04:20:30 +0300

    common editing of layout and correcting position of scrollToTop button

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
 var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 var rootElement = document.documentElement;
 
-function scrollToTop () {
-rootElement.scrollTo({
-    top: 0, 
-    behavior: "smooth"
-})
+function scrollToTop() {
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
 }
 
 scrollToTopBtn.addEventListener("click", scrollToTop);
